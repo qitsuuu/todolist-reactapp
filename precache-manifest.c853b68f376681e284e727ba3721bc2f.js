1e44307c58c3a8fb304a8c4f9789900c9a875462
qitsuuu <villoranteqit@gmail.com>
2019-05-16 16:44:52 +0800

    Updates

---
+++ precache-manifest.c853b68f376681e284e727ba3721bc2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c458d542a0f5838f245e",
+    "url": "/todolist-reactapp/static/js/runtime~main.444e296d.js"
+  },
+  {
+    "revision": "fbb96dffbc7012b6af7f",
+    "url": "/todolist-reactapp/static/js/main.faefe6e9.chunk.js"
+  },
+  {
+    "revision": "78b218299ff7c76a0e5a",
+    "url": "/todolist-reactapp/static/js/2.9d2f7a6b.chunk.js"
+  },
+  {
+    "revision": "fbb96dffbc7012b6af7f",
+    "url": "/todolist-reactapp/static/css/main.a58be1b1.chunk.css"
+  },
+  {
+    "revision": "4b2729c258cc841f7d543816146f902c",
+    "url": "/todolist-reactapp/index.html"
+  }
+];
