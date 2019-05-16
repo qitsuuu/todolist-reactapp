a7e512397bc4e665d4dc2262001a1372d4af2410
qitsuuu <villoranteqit@gmail.com>
2019-05-16 16:38:11 +0800

    updated some files for gh-pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 // import "./testing/test.css";
 import TaskList from "./components/TaskList";
 // import Testing from "./testing/Testing";
-import "mdbootstrap/css/bootstrap.css";
-import "mdbootstrap/css/mdb.css";
+// import "mdbootstrap/css/bootstrap.css";
+// import "mdbootstrap/css/mdb.css";
 
 ReactDOM.render(<TaskList />, document.getElementById("root"));
